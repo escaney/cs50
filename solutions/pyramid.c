86b2c6ed6a5a65e1d12c98754002f618c13cb04c
Elizabeth <escaney@icloud.com>
2021-01-04 16:39:57 +0000

    WIP: added comments and alterted code slightly

--- solutions/pyramid.c
+++ solutions/pyramid.c
@@ -6,25 +6,35 @@ void pyramid(int height);
 void create_hashes(int counter);
 
 int main(void) {
+  // here we receive back a value from get_user_height()
+  // And store it in the height variable
   int height = get_user_height();
+
+  // then we pass that value to our pyramid function
   pyramid(height);
 }
 
 int get_user_height(void) {
-  int num;
+  // setting num outside the do / while loop for scope reasons
+  int height;
 
+  // do keep asking for a valid height (a height that passes the condition)
   do 
   {
-    num = get_int("Height: ");
+    height = get_int("Height: ");
   }
-  while (num < 1);
+  while (height < 1);
 
-  return num;
+  // store that height inside this get_user_function
+  return height;
 }
 
 void create_hashes(int counter) {
+  // initiating i as 0, in order to iterate up to counter
   int i = 0;
 
+  // remember create_hashes() has been called INSIDE pyramid
+  // so while i is less than counter, keep printing # on one line
   while (i <= counter)
   {
     printf("#");
@@ -34,10 +44,14 @@ void create_hashes(int counter) {
 
 void pyramid(int height) {
   for (int counter = 0; counter < height; counter += 1) {
-    int i = height -1;
+    // this i is set so we can iterate down
+    int i = height;
 
     // increment and print spaces down from 'height' (i.e 5) to 0
-    while (i > counter)
+    // we need to +1 to counter -- It's a workaround to make
+    // the while loop run the appropriate amount of times
+    // ! It's weird, I know, don't worry bout it for now!
+    while (i > counter + 1)
     {
       printf(" ");
       i += -1;
